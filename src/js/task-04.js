1f112039a6e898c46a733f2647f3bc7a34494e39
Elżbieta Siewień <elzbieta.siewien@wp.pl>
2023-03-30 03:04:16 +0200

    poprawka1

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,5 +1,29 @@
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.appendChild(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-};
+}
+
+createBtn.addEventListener("click", () => createBoxes(input.value));
+destroyBtn.addEventListener("click", destroyBoxes);
